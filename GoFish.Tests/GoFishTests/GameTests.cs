bfb37d3033261dcddea44e4fd90adf384f6ba551
zbl14 <ifthereisoneday@gmail.com>
2022-07-21 16:39:42 -0700

    refatory Game Constructor, both player 1 and player 2 draw 5 cards, 42 cards left, test passed

--- GoFish.Tests/GoFishTests/GameTests.cs
+++ GoFish.Tests/GoFishTests/GameTests.cs
@@ -5,18 +5,18 @@ using GoFish.Models;
 namespace GoFish.Tests
 {
   [TestClass]
-  public class GameTests //: IDisposable
+  public class GameTests : IDisposable
   {
-    // public void Dispose()
-    // {
-    //   Game.ClearAll();
-    // }
+    public void Dispose()
+    {
+      Game.ClearAll();
+    }
 
     [TestMethod]
     public void GameConstructor_CreateInstanceOfGame_List()
     {
       Game newGame = new Game();
-      Assert.AreEqual(52, newGame.Deck.Count);
+      Assert.AreEqual(52, newGame.GetDeck().Count);
     }
 
     [TestMethod]
@@ -27,7 +27,21 @@ namespace GoFish.Tests
       Game newGame = new Game();
       newGame.Draw(num, player);
       Assert.AreEqual(num, newGame.GetHandOne().Count);
-      Assert.AreEqual(47, newGame.Deck.Count);
+      Assert.AreEqual(47, newGame.GetDeck().Count);
+    }
+
+    [TestMethod]
+    public void Draw_CreatListsOfNCards_Lists()
+    {
+      int num = 5;
+      string player1 = "player1";
+      string player2 = "player2";
+      Game newGame = new Game();
+      newGame.Draw(num, player1);
+      newGame.Draw(num, player2);
+      Assert.AreEqual(num, newGame.GetHandOne().Count);
+      Assert.AreEqual(num, newGame.GetHandTwo().Count);
+      Assert.AreEqual(42, newGame.GetDeck().Count);
     }
   }
 }
--- GoFish/Models/Game.cs
+++ GoFish/Models/Game.cs
@@ -5,18 +5,22 @@ namespace GoFish.Models
 {
   public class Game
   {
-    private static List<Card> _deck = Card.DeckBuilder();
+    private static List<Card> _deck = new List<Card> {};
     private static List<Card> _handOne = new List<Card> {}; 
-        private static List<Card> _handTwo = new List<Card> {};
-    public List<Card> Deck { get; }
+    private static List<Card> _handTwo = new List<Card> {};
     public int PlayerOneScore { get; set; }
     public int PlayerTwoScore { get; set; }
     
     public Game()
     {
-      Deck = _deck;
+      _deck = Card.DeckBuilder();
       PlayerOneScore = 0;
       PlayerTwoScore = 0;
+    }
+
+    public List<Card> GetDeck()
+    {
+      return _deck;
     }
 
     public List<Card> GetHandOne()
@@ -36,21 +40,27 @@ namespace GoFish.Models
       {
         for (int i = 0; i < num; i++)
         {
-          int rndCard = rnd.Next(Deck.Count);
-          _handOne.Add(Deck[rndCard]);
-          Deck.RemoveAt(rndCard);
+          int rndCard = rnd.Next(_deck.Count);
+          _handOne.Add(_deck[rndCard]);
+          _deck.RemoveAt(rndCard);
         }
       } 
-      else
+      else if (player == "player2")
       {
         for (int i = 0; i < num; i++)
         {
-          int rndCard = rnd.Next(Deck.Count);
-          _handTwo.Add(Deck[rndCard]);
-          Deck.RemoveAt(rndCard);
+          int rndCard = rnd.Next(_deck.Count);
+          _handTwo.Add(_deck[rndCard]);
+          _deck.RemoveAt(rndCard);
         }
       } 
     }
 
+    public static void ClearAll()
+    {
+      _deck.Clear();
+      _handOne.Clear();
+      _handTwo.Clear();
+    }
   }
 }
